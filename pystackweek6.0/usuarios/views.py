725fad71cc731840af417a729a786dc7c4d7038e
barbaraarruda <barbarahmiley@gmail.com>
2023-04-13 17:57:54 -0300

    finalização parte cadastro

--- pystackweek6.0/usuarios/views.py
+++ pystackweek6.0/usuarios/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.urls import reverse
+
 
 # Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem')
+            return redirect(reverse('cadastro'))
+
+        # validar força da senha - pesquisar e inserir nessa linha 
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'O usuário já existe')
+            return redirect(reverse('cadastro'))
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        messages.add_message(request, constants.SUCCESS, 'Usuário salvo com sucesso')
+        
+        return redirect(reverse('login'))
+    
