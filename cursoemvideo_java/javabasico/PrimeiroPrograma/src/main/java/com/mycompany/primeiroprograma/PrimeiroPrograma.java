1399ec38a9f42026060e01fcbfa51752d5ce9e4d
barbaraarruda <barbarahmiley@gmail.com>
2023-08-10 14:56:36 -0300

    início aulas de revisão em java

---
+++ cursoemvideo_java/javabasico/PrimeiroPrograma/src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package com.mycompany.primeiroprograma;
+/**
+ *
+ * @author barbara
+ */
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+}
