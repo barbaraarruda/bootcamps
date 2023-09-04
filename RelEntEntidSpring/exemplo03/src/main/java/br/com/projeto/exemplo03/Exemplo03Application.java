32d7f5c5b63be2c25126b3323a31d28c164d1e0b
barbaraarruda <barbarahmiley@gmail.com>
2023-09-04 08:42:00 -0300

    início curso de relacionamento entre entidades

---
+++ RelEntEntidSpring/exemplo03/src/main/java/br/com/projeto/exemplo03/Exemplo03Application.java
@@ -0,0 +1,13 @@
+package br.com.projeto.exemplo03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exemplo03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exemplo03Application.class, args);
+	}
+
+}
