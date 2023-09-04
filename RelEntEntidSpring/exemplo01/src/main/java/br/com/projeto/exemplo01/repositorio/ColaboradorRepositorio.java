5a6a407156575749256b3c8b5356af62ee225691
barbaraarruda <barbarahmiley@gmail.com>
2023-09-04 10:08:26 -0300

    aula 08 minutagem 0:32

---
+++ RelEntEntidSpring/exemplo01/src/main/java/br/com/projeto/exemplo01/repositorio/ColaboradorRepositorio.java
@@ -0,0 +1,9 @@
+package br.com.projeto.exemplo01.repositorio;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ColaboradorRepositorio  extends CrudRepository<Colaborador, Long>{
+    
+}
