9640b6232538752bcdd4a4e4a0d9b534b319dfe3
barbaraarruda <barbarahmiley@gmail.com>
2023-09-05 10:48:27 -0300

    teste finalizado de controle de postagens

---
+++ RelEntEntidSpring/exemplo02/src/main/java/br/com/projeto/exemplo02/modelo/Postagem.java
@@ -0,0 +1,33 @@
+package br.com.projeto.exemplo02.modelo;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_postagem")
+@Getter
+@Setter
+public class Postagem {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    private String titulo;
+
+    private String texto;
+
+    @OneToMany
+    @JoinColumn(name = "postagem_codigo")
+    private List<Comentario> comentarios;
+    
+}
